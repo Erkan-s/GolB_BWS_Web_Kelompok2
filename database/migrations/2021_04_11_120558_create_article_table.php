a20e197bbab9a36ea7ddfff8648eaee47dff39df
rifjan29 <rjndla@gmail.com>
2021-04-12 14:23:48 +0800

    Migration

---
+++ database/migrations/2021_04_11_120558_create_article_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('authors_id')->unsigned();
+            $table->string('title', 50);
+            $table->string('cover', 200)->nullable();
+            $table->text('desc');
+            $table->timestamps();
+            $table->foreign('authors_id')->references('id')->on('authors')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
