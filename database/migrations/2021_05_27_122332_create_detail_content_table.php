f4d40abb0c9b83ddcf87021357ee54c41d69791c
rifjan29 <rjndla@gmail.com>
2021-05-28 00:02:17 +0800

    Migration dan Seeds dan mengubah form login username atau email

---
+++ database/migrations/2021_05_27_122332_create_detail_content_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_content', function (Blueprint $table) {
+            $table->id();
+            $table->integer('content_id');
+            $table->unsignedTinyInteger('category_id');
+            $table->string('file')->nullable();
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_content');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TypeTableSeeder::class);
+        $this->call(UsersSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
