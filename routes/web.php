769740b8d8eaca98bf45a3a1f48c748584522a1d
rifjan29 <rjndla@gmail.com>
2021-06-01 15:32:42 +0800

    Template Backend

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('index');
 })->middleware(['auth'])->name('dashboard');
 
 // Route::middleware(['auth'])->group(function () {
